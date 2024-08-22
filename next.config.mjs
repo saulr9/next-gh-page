55c2039ff4ed2ad684cc01ac815f7869690a4037
Saul Ramos <seramos@cleveritgroup.com>
2024-08-22 14:45:05 -0400

    fix ci.yml

--- next.config.mjs
+++ next.config.mjs
@@ -2,22 +2,6 @@ const nextConfig = {
   assetPrefix: "/next-gh-page",
   basePath: "/next-gh-page",
   output: "export",
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: "next-swc-loader",
-            options: {
-              isServer,
-            },
-          },
-        ],
-      });
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
